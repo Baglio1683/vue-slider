d489f132a2e4b6f27352907728c0f53a6c377fdf
MyUsername <alberto.baglivi@gmail.com>
2022-10-26 14:43:05 +0200

    Add the infinite cicle

--- js/script.js
+++ js/script.js
@@ -1,17 +1,71 @@
 const {createApp} = Vue; 
 
 createApp({
-    
+
     data(){
 
-        return{
+        return{    
 
-        message : "Hello"
+        currentImage : 0,
 
+        slide : [
+            {
+                image: 'img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: 'img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: 'img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: 'img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: 'img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ]
 
         }
 
+    }, 
+
+    methods : {
+    
+    changeImagePrev : function (currentImage){
+     
+        if(this.currentImage > 0){
+        this.currentImage--
+        }
+        else{
+            this.currentImage = this.slide.length-1
+        }
+
+    }, 
+
+    changeImageNext : function(currentImage){
+
+        if(this.currentImage < this.slide.length-1){
+           this.currentImage++
+        }
+        else{
+          this.currentImage = 0 
+        }
+
     }
+
+}
+
 }).mount("#app")
 
 
